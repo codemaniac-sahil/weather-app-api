3f40d357f7fb947aad4ef4ef7865d90c873d762d
Sahil bisht <58036566+codemaniac-sahil@users.noreply.github.com>
2022-06-28 18:32:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function add_city(){
-let cityname=document.getElementById("data").value;
+let cityname=document.getElementById("data").value;          //Getting city name from user 
 api_url1="https://api.openweathermap.org/data/2.5/weather?q=";
 api_url2=cityname;
 api_url3="&appid=db8788ed4e52495bf8a51ae0a8e4caff&units=metric";
-full_api=api_url1+api_url2+api_url3;
+full_api=api_url1+api_url2+api_url3;                         //Generating URL for entered city 
+    
+    //If user did not input the city
 if (api_url2==''){
     document.getElementById('use').innerHTML="<p>Please enter the city</p>";
     document.getElementById('use1').innerText='';
@@ -14,11 +16,10 @@ if (api_url2==''){
 else{
 
 async function get_data(){
-const response=await fetch(full_api)
-const data=await response.json()
-tempe=data.main.temp;
+const response=await fetch(full_api)            //Fetching data from API
+const data=await response.json()                //Converting data to json format
+tempe=data.main.temp;                           
 city=data.name;
-error=data.cod;
 weather_condition=data.weather[0].main;
 imp=`<p> City : ${city}</p>`;
 imp1=`<p> Weather condition : ${weather_condition}</p>`;
