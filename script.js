2bc6e4894509f93a4b646857c47d5c362696d10a
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2022-10-02 13:40:27 +0300

    #2: Added more info about weather and searching by pressing enter (#3)
    * Added more info about weather and searching by pressing enter
    
    * Fixed gradient issues on scrollable page
    
    Co-authored-by: PolisanTheEasyNick <git@github.com>

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function add_city(){
+document.getElementById("data")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("tap").click();
+    }
+});
+
+function add_city() {
 let cityname=document.getElementById("data").value;          //Getting city name from user 
 api_url1="https://api.openweathermap.org/data/2.5/weather?q=";
 api_url2=cityname;
@@ -7,9 +15,9 @@ full_api=api_url1+api_url2+api_url3;                         //Generating URL fo
     
     //If user did not input the city
 if (api_url2==''){
-    document.getElementById('use').innerHTML="<p>Please enter the city</p>";
-    document.getElementById('use1').innerText='';
-    document.getElementById('use2').innerText='';
+    document.getElementById('cityName').innerHTML="<p>Please enter the city</p>";
+    document.getElementById('weatherCondition').innerText='';
+    document.getElementById('temperature').innerText='';
 
 
 }
@@ -20,30 +28,40 @@ const response=await fetch(full_api)            //Fetching data from API
 const data=await response.json()                //Converting data to json format
 tempe=data.main.temp;                           
 city=data.name;
+country=data.sys.country
 weather_condition=data.weather[0].main;
-imp=`<p> City : ${city}</p>`;
-imp1=`<p> Weather condition : ${weather_condition}</p>`;
-imp2=`<p> ${tempe}&#8451</p>`;
+pressure=data.main.pressure;
+humidity=data.main.humidity;
+windSpeed=data.wind.speed;
 
-document.getElementById('use').innerHTML=imp;
-document.getElementById('use1').innerHTML=imp1;
-document.getElementById('use2').innerHTML=imp2;
+cityName=`<p> City : ${city}, ${country}</p>`;
+weatCond=`<p> Weather condition : ${weather_condition}</p>`;
+temp=`<p> ${tempe}&#8451</p>`;
+press=`<p> Pressure : ${pressure} hPa</p>`;
+hum=`<p> Humidity : ${humidity}%</p>`
+windSpd=`<p> Wind speed : ${windSpeed}</p>`;
 
+document.getElementById('cityName').innerHTML=cityName;
+document.getElementById('weatherCondition').innerHTML=weatCond;
+document.getElementById('temperature').innerHTML=temp;
+document.getElementById('pressure').innerHTML=press;
+document.getElementById('humidity').innerHTML=hum;
+document.getElementById('windSpeed').innerHTML=windSpd;
 
 if(weather_condition=="Clouds"){
-    document.body.style="background-image:linear-gradient(lightblue,white);"
+    document.body.style="background-image:repeating-linear-gradient(lightblue,white);"
 }
 else if(weather_condition=="Thunderstorm"){
-    document.body.style="background-image:linear-gradient(white,gray);"
+    document.body.style="background-image:repeating-linear-gradient(white,gray);"
 }
 else if(weather_condition=="Rain"){
-    document.body.style="background-image:linear-gradient(lightblue,blue);"
+    document.body.style="background-image:repeating-linear-gradient(lightblue,blue);"
 }
 else if(weather_condition=="Snow"){
-    document.body.style="background-image:linear-gradient(#edebeb , #fffcfc);"
+    document.body.style="background-image:repeating-linear-gradient(#edebeb , #fffcfc);"
 }
 else{
-    document.body.style="background-image:linear-gradient(#edebeb , #fffcfc)";
+    document.body.style="background-image:repeating-linear-gradient(#edebeb , #fffcfc)";
 }
 
 
